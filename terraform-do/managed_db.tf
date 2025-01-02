6a693221d16e13dc47ffacfdf671b679293b8599
Ahmed Awadallah <aawadall@ualberta.ca>
2025-01-02 12:25:49 -0500

    add candidate schema

--- terraform-do/managed_db.tf
+++ terraform-do/managed_db.tf
@@ -2,7 +2,7 @@ resource "digitalocean_database_cluster" "mongo" {
   name = "job-board-mongo"
   node_count = 1
   engine = "mongodb"
-  version = "5"
+  version = "7"
   size = "db-s-1vcpu-1gb"
   region = var.region
   tags = [ "job-board", "database" ]
